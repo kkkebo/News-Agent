a5200a08ead36e066ff3c300d82e4e89e008d25c
Andrey.K <andrey-8991@yandex.ru>
2022-05-03 21:08:13 +0300

    Added base view-func and templates

--- djsitetest/news/models.py
+++ djsitetest/news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class News(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField(blank=True)
@@ -7,3 +8,6 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y%m%d/')
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
--- djsitetest/news/views.py
+++ djsitetest/news/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import News
 
 def index(request):
-    print(request)
-    return HttpResponse('Hello world')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'News List'
+    }
+    return render(request, 'news/index.html', context)
 
 
-def test(request):
-    return HttpResponse('<h1>Info-testpage</h1>')
